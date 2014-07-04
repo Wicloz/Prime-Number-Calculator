12199ecc01d29f3e49f19521bea9b6d3e36ad66e
Wicloz <deboer.wilco@gmail.com>
2014-07-04 18:05:00 +0200

    Now writes results to text file

--- src/main.java
+++ src/main.java
@@ -1,24 +1,26 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
 
-        String file = "Numbers.txt";
-//        BufferedWriter out = new BufferedWriter(new FileWriter(file));
+        BufferedWriter out = new BufferedWriter(new FileWriter("Numbers.txt"));
         calculations calculations = new calculations();
-        int number = 1;
+        int number = 0;
         boolean prime;
 
         while (true){
+            number++;
             calculations.primeFinder(number);
             prime = calculations.getPrime();
+
             if (prime){
                 System.out.println(number + " is a prime number.");
-//            out.write(number + " is a prime number");
+                out.write(number + " is a prime number");
+                out.newLine();
             }
-            number++;
         }
     }
 
