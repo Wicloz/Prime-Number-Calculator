a8e232067c8fb55f7ce1986323b44534d5b4f5b8
Wicloz <deboer.wilco@gmail.com>
2014-07-04 23:06:02 +0200

    Added writing to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int number = 0;
+    fstream file;
     bool prime;
+    int number = 0;
+    string line;
+    file.open("Numbers.txt");
 
     while (true){
         number++;
 
-                int dnumber = (number / 2);
-                prime = true;
-                if (number % 2 == 0 && number >= 10){
+            int dnumber = (number / 2);
+            prime = true;
+            if (number % 2 == 0 && number >= 10){
+                dnumber = 0;
+                prime = false;
+            }
+
+            while (dnumber > 1) {
+                int rest = number % dnumber;
+                if (rest == 0) {
                     dnumber = 0;
                     prime = false;
                 }
-
-                while (dnumber > 1) {
-                    int rest = number % dnumber;
-                    if (rest == 0) {
-                        dnumber = 0;
-                        prime = false;
-                    }
-                    dnumber--;
-                }
+                dnumber--;
+            }
 
         if (prime == true){
-        cout << number << " is a prime number." << endl ;
+        cout << number << " is a prime number." << endl;
+        file << number << " is a prime number." << endl;
         }
     }
 }
