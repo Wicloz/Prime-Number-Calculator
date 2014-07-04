0902f7269639c7ef851b075841cf124aa9053131
Wicloz <deboer.wilco@gmail.com>
2014-07-04 14:35:02 +0200

    Initial workspace

---
+++ src/calculations.java
@@ -0,0 +1,10 @@
+
+public class calculations {
+    public void primeFinder(){
+        double dnumber = number;
+        while (dnumber > 0){
+            int cnumber1 = number / dnumber;
+            double cnumber2 = number / dnumber;
+        }
+    }
+}
