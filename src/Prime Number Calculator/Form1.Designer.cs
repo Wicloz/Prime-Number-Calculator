d95b522f74ff1b755392f0139fbbd70f08224290
Wicloz <deboer.wilco@gmail.com>
2014-07-05 23:03:47 +0200

    Lots of code done and transferred

--- src/Prime Number Calculator/Form1.Designer.cs
+++ src/Prime Number Calculator/Form1.Designer.cs
@@ -33,13 +33,16 @@
             this.numberIn = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pNumberOut = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.addText = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // start
             // 
-            this.start.Location = new System.Drawing.Point(13, 13);
+            this.start.Location = new System.Drawing.Point(12, 13);
             this.start.Name = "start";
-            this.start.Size = new System.Drawing.Size(141, 54);
+            this.start.Size = new System.Drawing.Size(142, 54);
             this.start.TabIndex = 0;
             this.start.Text = "Start";
             this.start.UseVisualStyleBackColor = true;
@@ -47,9 +50,9 @@
             // 
             // stop
             // 
-            this.stop.Location = new System.Drawing.Point(13, 73);
+            this.stop.Location = new System.Drawing.Point(12, 73);
             this.stop.Name = "stop";
-            this.stop.Size = new System.Drawing.Size(141, 54);
+            this.stop.Size = new System.Drawing.Size(142, 54);
             this.stop.TabIndex = 1;
             this.stop.Text = "Stop";
             this.stop.UseVisualStyleBackColor = true;
@@ -57,16 +60,16 @@
             // 
             // numberIn
             // 
-            this.numberIn.Location = new System.Drawing.Point(16, 146);
+            this.numberIn.Location = new System.Drawing.Point(15, 156);
             this.numberIn.Name = "numberIn";
-            this.numberIn.Size = new System.Drawing.Size(138, 20);
+            this.numberIn.Size = new System.Drawing.Size(139, 20);
             this.numberIn.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 130);
+            this.label1.Location = new System.Drawing.Point(12, 140);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 3;
@@ -80,11 +83,32 @@
             this.pNumberOut.TabIndex = 4;
             this.pNumberOut.Text = "";
             // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(15, 232);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(142, 32);
+            this.saveFile.TabIndex = 5;
+            this.saveFile.Text = "Save to file";
+            this.saveFile.UseVisualStyleBackColor = true;
+            // 
+            // addText
+            // 
+            this.addText.AutoSize = true;
+            this.addText.Location = new System.Drawing.Point(15, 183);
+            this.addText.Name = "addText";
+            this.addText.Size = new System.Drawing.Size(69, 17);
+            this.addText.TabIndex = 6;
+            this.addText.Text = "Add Text";
+            this.addText.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(544, 276);
+            this.Controls.Add(this.addText);
+            this.Controls.Add(this.saveFile);
             this.Controls.Add(this.pNumberOut);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numberIn);
@@ -104,6 +128,9 @@
         private System.Windows.Forms.TextBox numberIn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox pNumberOut;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.CheckBox addText;
     }
 }
 
--- src/Prime Number Calculator/Form1.cs
+++ src/Prime Number Calculator/Form1.cs
@@ -17,13 +17,73 @@ namespace Prime_Number_Calculator
             InitializeComponent();
         }
 
+        public bool running;
+
         private void start_Click(object sender, EventArgs e)
         {
+            numberIn.Enabled = false;
+            int number = 0;
+            bool failConvert = false;
+            pNumberOut.Text = "";
 
+            try
+            {
+                number = Convert.ToInt32(numberIn.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                failConvert = true;
+            }
+
+            if (number > 0)
+            {
+                running = true;
+                pCalculator(number);
+            }
+            else if (failConvert != true)
+            {
+                MessageBox.Show("Invalid number.", "Error");
+            }
+
+            numberIn.Enabled = true;
         }
 
         private void stop_Click(object sender, EventArgs e)
         {
+            running = false;
+            numberIn.Enabled = true;
+        }
+
+        void pCalculator(int number)
+        {
+            while (number < 1000 && running == true)
+            {
+                int maxn = (number / 2);
+                bool prime = true;
+
+                for (int dnumber = 2; dnumber <= maxn; dnumber++)
+                {
+                    int rest = number % dnumber;
+                    if (rest == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+
+                if (prime == true)
+                {
+                    pNumberOut.Text += number.ToString();
+                    if (addText.Checked)
+                    {
+                        pNumberOut.Text += "  is a prime number.";
+                    }
+                    pNumberOut.Text += "    ";
+                }
+
+            number++;
+            }
 
         }
     }
