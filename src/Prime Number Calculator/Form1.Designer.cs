f0e2f270a72940b1bc13b01fca17ffa38187e16f
Wicloz <deboer.wilco@gmail.com>
2014-07-05 18:15:31 +0200

    Moving on to C#

---
+++ src/Prime Number Calculator/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Prime_Number_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.numberIn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pNumberOut = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(13, 13);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(141, 54);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(13, 73);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(141, 54);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // numberIn
+            // 
+            this.numberIn.Location = new System.Drawing.Point(16, 146);
+            this.numberIn.Name = "numberIn";
+            this.numberIn.Size = new System.Drawing.Size(138, 20);
+            this.numberIn.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start at:";
+            // 
+            // pNumberOut
+            // 
+            this.pNumberOut.Location = new System.Drawing.Point(172, 13);
+            this.pNumberOut.Name = "pNumberOut";
+            this.pNumberOut.Size = new System.Drawing.Size(360, 251);
+            this.pNumberOut.TabIndex = 4;
+            this.pNumberOut.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 276);
+            this.Controls.Add(this.pNumberOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberIn);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox numberIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox pNumberOut;
+    }
+}
+
---
+++ src/Prime Number Calculator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prime_Number_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
